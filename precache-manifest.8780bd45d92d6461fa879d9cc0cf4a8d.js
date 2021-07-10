0c8561c2c81e47c8bdf7090d36f5d4075282f1ec
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-10 15:42:43 +0530

    Updates

---
+++ precache-manifest.8780bd45d92d6461fa879d9cc0cf4a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d452cba5eac7fdc8281efe5ea8608c8d",
+    "url": "/Tabs-React-Project/index.html"
+  },
+  {
+    "revision": "6cdd3ca35f819a3ab451",
+    "url": "/Tabs-React-Project/static/css/main.70502fb5.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/Tabs-React-Project/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tabs-React-Project/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdd3ca35f819a3ab451",
+    "url": "/Tabs-React-Project/static/js/main.9244f03c.chunk.js"
+  },
+  {
+    "revision": "49e45b70abdcc4503d67",
+    "url": "/Tabs-React-Project/static/js/runtime-main.2d32c828.js"
+  }
+]);
