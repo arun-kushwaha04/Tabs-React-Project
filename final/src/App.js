551347f76f97ce0f9158a27394f0c12a6537fc89
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-10 11:48:12 +0530

    Added fetch function to fetch api

--- final/src/App.js
+++ final/src/App.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value,setValue] = useState(0);
+
+  //function to fetch api
+  const fetchJobs = () =>{
+    const response = await fetch(url);
+    const data = response.json();
+    setJobs(data);
+    setLoading(flase);
+  }
   return <h2>tabs project setup</h2>
 }
 
